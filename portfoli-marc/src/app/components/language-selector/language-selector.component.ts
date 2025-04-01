3b9f82b99fd7c884daea7224d76c2dd1b8716e59
Marc Fort <marcfort51@gmail.com>
2025-04-01 20:13:20 +0200

    Mejoras version movil

--- portfoli-marc/src/app/components/language-selector/language-selector.component.ts
+++ portfoli-marc/src/app/components/language-selector/language-selector.component.ts
@@ -1,33 +1,26 @@
-  import { Component } from '@angular/core';
-  import { TranslateService } from '@ngx-translate/core';
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
-  @Component({
-    selector: 'app-language-selector',
-    templateUrl: './language-selector.component.html',
-    styleUrls: ['./language-selector.component.scss']
-  })
-  export class LanguageSelectorComponent {
+@Component({
+  selector: 'app-language-selector',
+  templateUrl: './language-selector.component.html',
+  styleUrls: ['./language-selector.component.scss']
+})
+export class LanguageSelectorComponent {
+  languages = [
+    { code: 'cat', label: 'Català', flag: 'assets/flags/ca.png' },
+    { code: 'en', label: 'English', flag: 'assets/flags/en.png' },
+    { code: 'es', label: 'Español', flag: 'assets/flags/es.png' }
+  ];
 
-    // Llista d'idiomes disponibles amb el codi, etiqueta i ruta de la imatge de la bandera
-    languages = [
-      { code: 'cat', label: 'Català', flag: 'assets/flags/ca.png' },
-      { code: 'en', label: 'English', flag: 'assets/flags/en.png' },
-      { code: 'es', label: 'Español', flag: 'assets/flags/es.png' }
-    ];
-
-    constructor(private translate: TranslateService) {
-      // Afegeix els idiomes disponibles
-      translate.addLangs(this.languages.map(lang => lang.code));
-      // Defineix l'idioma per defecte
-      translate.setDefaultLang('en');
-      // Opcional: detecta l'idioma del navegador i l'utilitza si està disponible
-      const browserLang = this.translate.getBrowserLang() || 'en';
-      this.translate.use(this.languages.some(lang => lang.code === browserLang) ? browserLang : 'en');
-      
-    }
-
-    // Mètode per canviar l'idioma quan es fa clic a una bandera
-    changeLanguage(lang: string) {
-      this.translate.use(lang);
-    }
+  constructor(private translate: TranslateService) {
+    translate.addLangs(this.languages.map(lang => lang.code));
+    translate.setDefaultLang('en');
+    const browserLang = this.translate.getBrowserLang() || 'en';
+    this.translate.use(this.languages.some(lang => lang.code === browserLang) ? browserLang : 'en');
   }
+
+  changeLanguage(lang: string) {
+    this.translate.use(lang);
+  }
+}
